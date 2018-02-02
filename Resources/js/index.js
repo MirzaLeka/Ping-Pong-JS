a8646ae1cbef4ef25267af860cae5f0073f30310
SPY4ER <mirza_376@hotmail.com>
2018-02-02 23:59:16 +0100

    Bouncing ball added

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,30 +1,72 @@
 
-
-
 var canvas = document.getElementById("gameCanvas");
 var canvasContext;
+
 var ballX = 50;
+var ballSpeedX = -20;
+
+var ballY = 50;
+var ballSpeedY = -20;
+
 var framesPerSecond = 60;
+
+var audio = new Audio('../Resources/audio/bouncingBall.mp3');
 
 window.onload = function() {
 
-setInterval(function(){ movement(); graphics();} , 1000/framesPerSecond);
+ canvasContext = canvas.getContext("2d"); 
+ /* Background Canvas */    
+
+setInterval(function(){ motion(); graphics();} , 50);
+
 
 }
 
-function movement() {
-//   ballX+=5;
+function motion() {
 
+/* Motion X Direction */
+
+ ballX = ballX + ballSpeedX;
+
+if (ballX > canvas.width) {
+ballSpeedX = -ballSpeedX;
+audio.play();
 }
 
+
+if (ballX < 0) {
+ballSpeedX = -ballSpeedX;
+audio.play();
+}
+
+
+/* Motion Y Direction */
+
+ballY = ballY + ballSpeedY;
+
+if (ballY > canvas.height) {
+ballSpeedY = -ballSpeedY;
+audio.play();
+}
+
+if (ballY < 0) {
+ballSpeedY = -ballSpeedY;
+audio.play();
+}
+
+
+
+}
 
 function graphics() {
 
-    canvasContext = canvas.getContext("2d"); 
-    
+ 
+
+/* Background Canvas */    
+canvasContext.fillStyle = "black";
+canvasContext.fillRect(0,0, canvas.width, canvas.height);
 
  /* Middle Line */   
-
 var offsetTop = 0;
 
 for (var i = 0; i < 11; i++) {
@@ -34,13 +76,13 @@ for (var i = 0; i < 11; i++) {
 }
 
 /* Left Paddle */
-    createCanvas(20,250,15,100,'white');
+ createCanvas(20,250,15,100,'white');
 
 /* Right Paddle */
 createCanvas(765,250,15,100,'white');
 
 /* Ball */
-createBall(350, 250, 10, 'white');
+createBall(ballX, ballY, 10, 'red');
 
 }
 
