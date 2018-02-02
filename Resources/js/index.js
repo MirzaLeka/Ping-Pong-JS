af61f153b08eeaa25d3e611ab895f4b941d4fbd1
SPY4ER <mirza_376@hotmail.com>
2018-02-02 19:49:59 +0100

    Ball added. Code simplified.

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -8,7 +8,7 @@ var framesPerSecond = 60;
 
 window.onload = function() {
 
-setInterval(function(){ movement(); graphics();} ,framesPerSecond);
+setInterval(function(){ movement(); graphics();} , 1000/framesPerSecond);
 
 }
 
@@ -18,32 +18,42 @@ function movement() {
 }
 
 
-function graphics() { 
+function graphics() {
+
+    canvasContext = canvas.getContext("2d"); 
     
 
  /* Middle Line */   
-canvasContext = canvas.getContext("2d");
-canvasContext.fillStyle = "white";
 
 var offsetTop = 0;
 
 for (var i = 0; i < 11; i++) {
 
-    canvasContext.fillRect(390,offsetTop,10,40); //positionX, positionY, width, height
+    createCanvas(392,offsetTop,8,40,'white');
     offsetTop+=80;
 }
 
 /* Left Paddle */
-canvasContext.fillStyle = "white";
-canvasContext.fillRect(20, 250, 15, 100);
-
+    createCanvas(20,250,15,100,'white');
 
 /* Right Paddle */
-canvasContext.fillStyle = "white";
-canvasContext.fillRect(765, 250, 15, 100);
+createCanvas(765,250,15,100,'white');
 
+/* Ball */
+createBall(350, 250, 10, 'white');
 
+}
 
+function createCanvas(positionX, positionY, width, height, color){
+canvasContext.fillStyle = color;
+canvasContext.fillRect(positionX, positionY, width, height);
 
+}
+
+function createBall(positionX, positionY, radius, color) {
+canvasContext.fillStyle = color;
+canvasContext.beginPath();
+canvasContext.arc(positionX, positionY, radius, 0, Math.PI*2, true);
+canvasContext.fill();
 }
 
